07fc5c06f3f27202a476acd3c8f04d9e332fe4ec
Hacks4Snacks <mardaltongray@gmail.com>
2024-10-08 12:26:29 -0500

    uplift trivy to 0.56.1

--- pkg/plugins/trivy/plugin.go
+++ pkg/plugins/trivy/plugin.go
@@ -81,7 +81,7 @@ func (p *plugin) Init(ctx trivyoperator.PluginContext) error {
 	return ctx.EnsureConfig(trivyoperator.PluginConfig{
 		Data: map[string]string{
 			keyTrivyImageRepository:           DefaultImageRepository,
-			keyTrivyImageTag:                  "0.52.2",
+			keyTrivyImageTag:                  "0.56.1",
 			KeyTrivySeverity:                  DefaultSeverity,
 			keyTrivySlow:                      "true",
 			keyTrivyMode:                      string(Standalone),
